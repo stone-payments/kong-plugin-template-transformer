82eea9492181becc090238cb8ae777e65f8c2868
Bianca Rosa <me@biancarosa.com.br>
2018-04-12 17:33:17 -0300

    :heavy_plus_sign: add template transformer

--- template-transformer/handler.lua
+++ template-transformer/handler.lua
@@ -1,16 +1,13 @@
 local BasePlugin = require 'kong.plugins.base_plugin'
-local http = require 'resty.http'
-local cjson = require 'cjson'
 local template = require 'resty.template'
 
-local req_read_body = ngx.req.read_body
 local req_set_body_data = ngx.req.set_body_data
 local req_get_body_data = ngx.req.get_body_data
+local req_get_uri_args = ngx.req.get_uri_args
 local req_set_header = ngx.req.set_header
-local log = ngx.log
+local req_get_headers = ngx.req.get_headers
+local req_read_body = ngx.req.read_body
 local TemplateTransformerHandler = BasePlugin:extend()
-
-TemplateTransformerHandler.PRIORITY = 1006
 
 function TemplateTransformerHandler:new()
   TemplateTransformerHandler.super.new(self, 'template-transformer')
@@ -18,12 +15,14 @@ end
 
 function TemplateTransformerHandler:access(config)
   TemplateTransformerHandler.super.access(self)
-  log(ngx.NOTICE, string.format("Template :: %s", config.template))
+  ngx.log(ngx.NOTICE, string.format("Template :: %s", config.template))
   local compiled_template = template.compile(config.template)
   req_read_body()
-  -- local body = req_get_body_data()
-  local transformed_body = compiled_template{affiliationCode = "123456789" }
-  log(ngx.NOTICE, string.format("Rendered Template :: %s", transformed_body))
+  local body = req_get_body_data()
+  local headers = req_get_headers()
+  local query_string = req_get_uri_args()
+  local transformed_body = compiled_template{query_string = query_string, headers = headers, body = body}
+  ngx.log(ngx.NOTICE, string.format("Rendered Template :: %s", transformed_body))
   req_set_body_data(transformed_body)
   req_set_header(CONTENT_LENGTH, #transformed_body)
 end
