b5265e19d3fde88a72b8ef0e4b45792aa61de99d
Bianca Rosa <me@biancarosa.com.br>
2018-04-12 17:47:01 -0300

    :white_check_mark: add more tests

--- template-transformer/handler_spec.lua
+++ template-transformer/handler_spec.lua
@@ -1,10 +1,10 @@
 local ngx =  {
     req = {
         set_body_data = spy.new(function() end),
-        get_body_data = spy.new(function() end),
-        get_uri_args = spy.new(function() end),
+        get_body_data =  spy.new(function() return { data = "oi2" } end),
+        get_uri_args = spy.new(function() return { query = "oi1" } end),
         set_header = spy.new(function() end),
-        get_headers = spy.new(function() end),
+        get_headers = spy.new(function() return { my_cool_header = "oi3" } end),
         read_body = spy.new(function() end),
     },
     config = {
@@ -34,5 +34,14 @@ describe("TestHandler", function()
     }
     TemplateTransformerHandler:access(config)
     assert.spy(ngx.req.set_body_data).was_called_with(config.template)
+  end)
+  
+  it("should test replace with template with variables", function()
+    TemplateTransformerHandler:new()
+    local config = {
+        template = "{{query_string['query']}} {{body['data']}} {{headers['my_cool_header']}}"
+    }
+    TemplateTransformerHandler:access(config)
+    assert.spy(ngx.req.set_body_data).was_called_with("oi1 oi2 oi3")
   end)
 end)
