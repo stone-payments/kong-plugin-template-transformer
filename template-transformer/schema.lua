c29e9ac5ac8b5e7c185d949fecf9064d94270269
Bianca Rosa <me@biancarosa.com.br>
2018-04-13 13:30:42 -0300

    :white_check_mark: add schema tests

--- template-transformer/schema.lua
+++ template-transformer/schema.lua
@@ -1,7 +1,32 @@
+local template = require 'resty.template'
+
+function check_template(string_template)
+  if string_template then
+    local status, err = pcall(function () 
+      template.precompile(string_template) 
+    end)
+    return status
+  end
+  return true
+end
+
 return {
   no_consumer = true,
   fields = {
-    request_template = {type = "string", required = false}
-    response_template = {type = "string", required = false}
+    request_template = {
+      type = "string", 
+      required = false,
+      self_check = function(schema, config, dao, is_updating)
+        -- print(config.request_template)
+        return check_template(config.request_template)
+      end
+    },
+    response_template = {
+      type = "string", 
+      required = false,
+      self_check = function(schema, config, dao, is_updating)
+        return check_template(config.response_template)
+      end
+    }
   }
 }
