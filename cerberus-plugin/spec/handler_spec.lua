e031c7a75d68fef82906b6f6cda9341b7b2e001a
Fadel <pfadel@stone.com.br>
2018-04-13 13:48:14 -0300

    :white_check_mark: adiciona testes unitários

--- cerberus-plugin/spec/handler_spec.lua
+++ cerberus-plugin/spec/handler_spec.lua
@@ -1,35 +1,77 @@
 -- Mock ngx
 local ngx =  {
     req = {
-        set_body_data = spy.new(function() end),
-        get_body_data =  spy.new(function() return { data = "oi2" } end),
-        get_uri_args = spy.new(function() return { query = "oi1" } end),
-        set_header = spy.new(function() end),
-        get_headers = spy.new(function() return { my_cool_header = "oi3" } end),
-        read_body = spy.new(function() end),
+        get_method = spy.new(function() end),
+        get_body_data =  spy.new(function() end),
+        get_uri_args = spy.new(function() end),
+        get_headers = spy.new(function() end),
     },
     config = {
-        prefix = spy.new(function()
-            return "mock"
-        end),
         ngx_lua_version = "1"
     },
-    location = {
-        capture = spy.new(function() end)
-    },
-    get_phase = spy.new(function() end),
     log = spy.new(function() end),
-    timer = spy.new(function() end),
-    re = spy.new(function() end),
-    socket = spy.new(function() end)
+    timer = {
+        at = spy.new(function() end)
+    },
+    re = {match = spy.new(function() return {"match"} end)},
+    socket = {
+        tcp = spy.new(function()
+                        return {
+
+                      }
+                      end)
+        },
+    var = {scheme = "http"}
 }
 _G.ngx = ngx
+
+-- Mock http
+local http =  {
+    new  = spy.new(function() end),
+    request_uri  = spy.new(function() end)
+}
 local LogRequestHandler = require('../handler')
 
 -- tests
 describe("TestHandler", function()
+
+  setup(function()
+    LogRequestHandler._httpc = http
+  end)
+
   it("should test handler constructor", function()
     LogRequestHandler:new()
     assert.equal("cerberus-plugin", LogRequestHandler._name)
   end)
+
+  it("should test call of ngx timer", function()
+    LogRequestHandler:new()
+    assert.equal("cerberus-plugin", LogRequestHandler._name)
+    config = {
+        url = "url",
+        product_company = "productcompany",
+        product_name = "productname",
+        product_version = "product_version",
+        tags = {}
+    }
+    LogRequestHandler:access(config)
+    assert.spy(ngx.req.get_method).was_called_with()
+    assert.spy(ngx.req.get_headers).was_called_with()
+    assert.spy(ngx.req.get_uri_args).was_called_with()
+    assert.spy(ngx.req.get_body_data).was_called_with()
+    assert.spy(ngx.timer.at).was_called(1)
+  end)
+
+  it("should test log function does not executes http request", function()
+    LogRequestHandler:new()
+    log(1, "http://mock.com", "payload", http)
+    assert.spy(http.request_uri).was_called(0)
+  end)
+
+  it("should test log function executes http request", function()
+    LogRequestHandler:new()
+    log(nil, "http://mock.com", "payload", http)
+    assert.spy(http.request_uri).was_called(1)
+  end)
+
 end)
