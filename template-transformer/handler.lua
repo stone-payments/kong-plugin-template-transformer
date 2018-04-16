012923a500d6bc438b79f09c1acba5d3ba561755
Bianca Rosa <me@biancarosa.com.br>
2018-04-16 11:14:16 -0300

    :bug: fix more bugs

--- template-transformer/handler.lua
+++ template-transformer/handler.lua
@@ -26,12 +26,12 @@ local function read_json_body(body)
 end
 
 local function prepare_body(body)
-  local v = cjson_encode(transformed_body)
+  local v = cjson_encode(body)
   if sub(v, 1, 1) == [["]] and sub(v, -1, -1) == [["]] then
     v = gsub(sub(v, 2, -2), [[\"]], [["]]) -- To prevent having double encoded quotes
   end
   v = gsub(v, [[\/]], [[/]]) -- To prevent having double encoded slashes
-  ngx.log(ngx.DEBUG, string.format("Encoded Body :: %s", v))
+  ngx.log(ngx.NOTICE, string.format("Encoded Body :: %s", v))
   return v
 end
 
@@ -46,8 +46,8 @@ function TemplateTransformerHandler:access(config)
     local body = req_get_body_data()
     local headers = req_get_headers()
     local query_string = req_get_uri_args()
-    local args = {query_string = query_string, headers = headers, body = body}
-    local transformed_body = template_transformer.transform(config.request_template, args)
+    local transformed_body = template_transformer.get_template(config.request_template){query_string = query_string, headers = headers, body = body}
+    ngx.log(ngx.NOTICE, string.format("Transformed Body :: %s", transformed_body))
     req_set_body_data(transformed_body)
     req_set_header(CONTENT_LENGTH, #transformed_body)
   end
@@ -64,7 +64,6 @@ end
 
 function TemplateTransformerHandler:body_filter(config)
   TemplateTransformerHandler.super.body_filter(self)
-  ngx.log(ngx.DEBUG, string.format("config.response_templat :: %s", config.response_template))
   if config.response_template then
     local chunk, eof = ngx.arg[1], ngx.arg[2]
     if not eof then
@@ -76,11 +75,11 @@ function TemplateTransformerHandler:body_filter(config)
       ngx.arg[1] = nil
     else
       -- body is fully read
+      ngx.log(ngx.NOTICE, string.format("Body :: %s", ngx.ctx.buffer))
       local body = read_json_body(ngx.ctx.buffer)
-      ngx.log(ngx.DEBUG, string.format("Body :: %s", body))
       local headers = res_get_headers()
-      local transformed_body = template_transformer.transform(config.response_template, {headers = headers, body = body})
-      ngx.log(ngx.DEBUG, string.format("Transformed Body :: %s", transformed_body))
+      local transformed_body = template_transformer.get_template(config.response_template){headers = headers, body = body}
+      ngx.log(ngx.NOTICE, string.format("Transformed Body :: %s", transformed_body))
       ngx.arg[1] = prepare_body(transformed_body)
     end
   end
--- template-transformer/template_transformer.lua
+++ template-transformer/template_transformer.lua
@@ -2,16 +2,13 @@ local resty_template = require 'resty.template'
 
 _M = {}
 
-_M.transform = function(template, ...)
-    ngx.log(ngx.DEBUG, string.format("Template :: %s", template))
+_M.get_template = function(template, args)
+    ngx.log(ngx.NOTICE, string.format("Template :: %s", template))
     local compiled_template = resty_template.compile(template)
-    local args = ...
     if not args then
         args = {}
     end
-    local transformed_body = compiled_template{query_string = args.query_string, headers = args.headers, body = args.body}
-    ngx.log(ngx.DEBUG, string.format("Rendered Template :: %s", transformed_body))
-    return transformed_body
+    return compiled_template
 end
 
 return _M
