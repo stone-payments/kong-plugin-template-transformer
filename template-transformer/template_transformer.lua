7f60ddc19d9f7022a1143b06a69cb8a5df957199
Bianca Rosa <me@biancarosa.com.br>
2018-04-16 13:30:04 -0300

    remove args

--- template-transformer/template_transformer.lua
+++ template-transformer/template_transformer.lua
@@ -2,12 +2,9 @@ local resty_template = require 'resty.template'
 
 _M = {}
 
-_M.get_template = function(template, args)
+_M.get_template = function(template)
     ngx.log(ngx.NOTICE, string.format("Template :: %s", template))
     local compiled_template = resty_template.compile(template)
-    if not args then
-        args = {}
-    end
     return compiled_template
 end
 
