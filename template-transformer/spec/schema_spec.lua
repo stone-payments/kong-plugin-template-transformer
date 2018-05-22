47ea09eac52cead0ae6a49a25d0502f634afbd5b
Fadel <pfadel@stone.com.br>
2018-05-22 17:16:10 -0300

    :lipstick: improves tests descriptions

--- template-transformer/spec/schema_spec.lua
+++ template-transformer/spec/schema_spec.lua
@@ -16,7 +16,7 @@ _G.ngx = ngx
 local schema = require('schema')
 
 describe("TestSchema", function()
-    it("should test that schema is initialized", function()
+    it("should initialize schema correctly", function()
       assert.equal(schema.no_consumer, true)
       assert.not_nil(schema.fields.request_template)
       assert.not_nil(schema.fields.request_template.type, "string")
@@ -26,7 +26,7 @@ describe("TestSchema", function()
       assert.not_nil(schema.fields.request_template.required, false)
     end)
 
-    it("should test request_template self check with valid template", function()
+    it("should return false then the request template is invalid", function()
       local string_template = "hello im a wrong {{a['}} template"
 
       local valid = schema.self_check(nil, { request_template = string_template }, nil, nil)
@@ -34,7 +34,7 @@ describe("TestSchema", function()
       assert.is_false(valid)
     end)
 
-    it("should test request_template self check with valid template", function()
+    it("should return true then the request template is valid", function()
       local string_template = "hello im a correct {{a}} template"
 
       local valid = schema.self_check(nil, { request_template = string_template }, nil, nil)
@@ -42,7 +42,7 @@ describe("TestSchema", function()
       assert.is_true(valid)
     end)
 
-    it("should test response_template self check with valid template", function()
+    it("should return false then the response template is invalid", function()
       local string_template = "hello im a wrong {{a['}} template"
 
       local valid = schema.self_check(nil, { response_template = string_template }, nil, nil)
@@ -50,7 +50,7 @@ describe("TestSchema", function()
       assert.is_false(valid)
     end)
 
-    it("should test response_template self check with valid template", function()
+    it("should return true then the response template is valid", function()
       local string_template = "hello im a correct {{a}} template"
 
       local valid = schema.self_check(nil, { response_template = string_template }, nil, nil)
