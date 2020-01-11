2d87f7fd95ef4f5f684d13d38cf698e95e8469aa
Henrique Escobar Hornos <henrique.hornos_ext@stone.com.br>
2020-01-11 16:16:19 -0300

    :art: allows the use of json objects in the template
    - example: { "data": "{{ body }}" } or subfields json objects { "data": { "foo": "{{ body.foo }}"

--- template-transformer/spec/handler_spec.lua
+++ template-transformer/spec/handler_spec.lua
@@ -307,7 +307,7 @@ describe("Test TemplateTransformerHandler body_filter", function()
     assert.equal('{ "data" : "bar" }', ngx.arg[1])
   end)
 
-  it("should build first ngx arg correctly when body is fully read with table variables", function()
+  it("should build first ngx arg correctly when body is fully read with table body variables", function()
     TemplateTransformerHandler:new()
     mock_resp_headers = {}
     table_data = {
@@ -321,10 +321,30 @@ describe("Test TemplateTransformerHandler body_filter", function()
     json_data = cjson_encode(table_data)
     _G.ngx.ctx.buffer = json_data
     local config = {
-        response_template = '{"data":"{{body}}"}'
+        response_template = '{"data":"{{ body }}"}'
     }
     TemplateTransformerHandler:body_filter(config)
     assert.equal('{"data":{"foo":{"bar":["into_bar",1,2,3],"bar1":"into_bar1"},"fin":"finish"}}', ngx.arg[1])
+  end)
+
+  it("should build first ngx arg correctly when body is fully read with table any variables", function()
+    TemplateTransformerHandler:new()
+    mock_resp_headers = {}
+    table_data = {
+      foo = {
+        bar = {"into_bar", 1, 2, 3},
+        bar1 = "into_bar1"
+      },
+      fin = "finish"
+    }
+
+    json_data = cjson_encode(table_data)
+    _G.ngx.ctx.buffer = json_data
+    local config = {
+        response_template = '{"data":"{"foo":"{{ body.foo }}","foo2":"{{ body.foo.bar }}"}'
+    }
+    TemplateTransformerHandler:body_filter(config)
+    assert.equal('{"data":"{"foo":{"bar":["into_bar",1,2,3],"bar1":"into_bar1"},"foo2":["into_bar",1,2,3]}', ngx.arg[1])
   end)
 
   it("should build first ngx arg correctly when template mapps to empty string", function()
--- template-transformer/template_transformer.lua
+++ template-transformer/template_transformer.lua
@@ -6,14 +6,50 @@ local cjson_encode = require('cjson').encode
 _M = {}
 
 _M.get_template = function(template, body)
-    if string.find(template, '"{{body}}"') then
-        data = cjson_encode(body)
-        template = string.gsub(template, "\"{{body}}\"", data)
-        string.gsub(template, '"{{body}}"', data)
-        return resty_template.compile(template)
-    else
-        return resty_template.compile(template)
+    fields = {}
+
+    -- Obtem todos os campos do template
+    for field in string.gmatch(template, '{{(.-)}}') do
+        field = string.gsub(field, " ", "")
+        table.insert(fields, field)
     end
+
+    -- percorre campos template e substitui tags pelo objeto json caso seja do tipo table
+    for idx, field in pairs(fields) do
+        -- atribui a tabela inteira do body na variavel
+        var = body
+
+        -- somente substitui tags iniciadas em body para manter compatibilidade com resty templates
+        x_body, y_body = string.find(field, "body")
+        x_point, y_point = string.find(field, "body%.")
+        x_total, y_total = string.find(field, "body.")
+
+        if ((x_body == 1 and y_body == 4) and not x_point and not y_point and not x_total and not y_total) or
+            ((x_body == 1 and y_body == 4) and (x_point == 1 and y_point == 5) and (x_total == 1 and y_total == 5)) then
+            f = {}
+
+            -- separa campos de cada tag contendo .
+            for value in string.gmatch(field, '([^.]+)') do
+                -- elimina o campo body dessa lista
+                if not string.find(value, "body") then
+                    table.insert(f, value)
+                end
+            end
+            
+            -- verifica se se expande body inteiro ou se filtra campos do body de acordo com a tag
+            if #f > 0 then
+                for i, d in pairs(f) do
+                    var = var[d]
+                end
+            end
+
+            if type(var) == 'table' then
+                data = cjson_encode(var)
+                template, ret = string.gsub(template, string.format('\"{{ %s }}\"', field), data)
+            end
+        end
+    end
+    return resty_template.compile(template)
 end
 
 return _M
