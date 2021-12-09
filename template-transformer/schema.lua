411d5912b230a5200270f567fdd4aaf03eb6cd98
Betim <gvampre1991@gmail.com>
2021-12-09 19:49:22 -0300

    chore: Update schema

--- template-transformer/schema.lua
+++ template-transformer/schema.lua
@@ -1,35 +1,6 @@
 local typedefs = require "kong.db.schema.typedefs"
-local template = require 'resty.template'
 
-function check_template(schema, config, dao, is_updating)
-  if config.request_template then
-    local status, err = pcall(function ()
-      template.precompile(config.request_template)
-    end)
-
-    if status ~= true then
-      return false, err
-    end
-
-    return status, err
-  end
-
-  if config.response_template then
-    local status, err = pcall(function ()
-      template.precompile(config.response_template)
-    end)
-
-    if status ~= true then
-      return false, err
-    end
-
-    return status, err
-  end
-
-  return true
-end
-
-local _M = {
+return {
   name = "kong-plugin-template-transformer",
   fields = {
     { consumer = typedefs.no_consumer, },
@@ -46,13 +17,3 @@ local _M = {
     }
   },
 }
-
-local function constructor()
-  return setmetatable({
-    self_check = check_template
-  }, { __index = _M })
-end
-
-setmetatable(_M, { __call = constructor })
-
-return _M
