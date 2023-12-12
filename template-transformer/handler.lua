f235bf4879c1e220fc9130d1cec548c7636d416b
Jonathan Lazaro <jonathan.lazaro1@gmail.com>
2023-12-12 10:25:28 -0400

    chore(release): 1.4.1

--- template-transformer/handler.lua
+++ template-transformer/handler.lua
@@ -16,7 +16,7 @@ local gsub = string.gsub
 local gmatch = string.gmatch
 local TemplateTransformerHandler = {
   PRIORITY = 801,
-  VERSION = "1.4.0"
+  VERSION = "1.4.1"
 }
 
 local template_transformer = require 'kong.plugins.kong-plugin-template-transformer.template_transformer'
